692b8fda82b7f94e307f250a6ca24d2dbb619f7b
Tianyi Zhong <zhongtianyi26@LAPTOP-2U5GPUET>
2024-04-22 10:15:10 -0500

    add Route

---
+++ src/main/java/com/example/cs411_final_project/entity/Route.java
@@ -0,0 +1,64 @@
+package com.example.cs411_final_project.entity;
+
+public class Route {
+    private String flightNumber;
+    private int AirlineId;
+    private int departureAirportID;
+    private int arrivalAirportID;
+    private int stops;
+
+    // 构造器
+    public Route() {
+    }
+
+    public Route(String flightNumber, int AirlineId, int departureAirportID, int arrivalAirportID, int stops) {
+        this.flightNumber = flightNumber;
+        this.AirlineId = AirlineId;
+        this.arrivalAirportID = arrivalAirportID;
+        this.departureAirportID = departureAirportID;
+        this.stops = stops;
+
+    }
+
+    // Getter 和 Setter 方法
+    public String getflightNumber() {
+        return flightNumber;
+    }
+
+    public void setflightNumber(String flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+    public int getAirlineId() {
+        return AirlineId;
+    }
+
+    public void setAirlineId(int AirlineId) {
+        this.AirlineId = AirlineId;
+    }
+
+    public int getdepartureAirportID() {
+        return departureAirportID;
+    }
+
+    public void setdepartureAirportID(int departureAirportID) {
+        this.departureAirportID = departureAirportID;
+    }
+
+    public int getarrivalAirportID() {
+        return arrivalAirportID;
+    }
+
+    public void setarrivalAirportID(int arrivalAirportID) {
+        this.arrivalAirportID = arrivalAirportID;
+    }
+
+    public int getstops() {
+        return stops;
+    }
+
+    public void setstops(int stops) {
+        this.stops = stops;
+    }
+
+}
